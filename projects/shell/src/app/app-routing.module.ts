bc192f1730018c0bd6a33b4527c175611bb6c8b3
Brolo <brolo1341@gmail.com>
2023-04-25 15:46:46 +0300

    added module federation in every projects

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  pathMatch: 'full'
+}, {
+  path: 'books',
+  loadChildren: () => import('mfe1/Module').then(m => m.BookModule)
+}, {
+  path: 'auth',
+  loadChildren: () => import('mfe2/Module').then(m => m.AuthModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
