2bb2de5f6f4b23789d9d43282f15db8f7a76ffff
Brolo <brolo1341@gmail.com>
2023-04-26 18:27:07 +0300

    added ngrx effects store debtools

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { StoreModule } from '@ngrx/store';
+import { metaReducers, reducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,10 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/shell/src/app/reducers/index.ts
@@ -0,0 +1,11 @@
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+
+export interface State {
+  // Define your state properties here
+}
+
+export const reducers: ActionReducerMap<State> = {
+  // Define your reducers here
+};
+
+export const metaReducers: MetaReducer<State>[] = [];
